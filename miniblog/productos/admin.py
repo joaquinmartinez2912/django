76a4b1de78691d3442679aff00baac1b31f83609
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-03-29 20:19:44 -0300

    idioma y detalles para el visualizar el administrador

--- miniblog/productos/admin.py
+++ miniblog/productos/admin.py
@@ -7,3 +7,9 @@ from productos.models import Product
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "price", "description")
+    #date_hierarchy = "created"
+
+
+# class ProductAdmin(admin.ModelAdmin):
+#      readonly_fields = ("created","modified")
+# Despues habria que poner a esta clase como parametro  en fila 7
--- miniblog/productos/models.py
+++ miniblog/productos/models.py
@@ -5,9 +5,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    #created = models.DateTimeField(auto_now_add=True)
+    #modified = models.DateTimeField(auto_now=True)
+
+    # class Meta:
+    #     verbose_name = "Producto"
+    #     verbose_name_plural = "Productos"
 
     def __str__(self) -> str:
         return self.name
+    
     
     # def save(self, *args, **kwargs):
     #     if self.price>0:
