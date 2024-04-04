6f74c86dc9bd781dbe118947a6e91067ec0ff093
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-04-03 22:03:41 -0300

    claves foraneas y ORM

--- miniblog/productos/admin.py
+++ miniblog/productos/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from productos.models import Product
+from productos.models import Product, Category
 
 # Para registrar y poder trabajar desde el administrador
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "description")
+    list_display = ("name", "price", "description","category")
     #date_hierarchy = "created"
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
 
 
 # class ProductAdmin(admin.ModelAdmin):
--- miniblog/productos/models.py
+++ miniblog/productos/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category,
+        on_delete = models.SET_NULL,
+        related_name="products",
+        null=True, blank=True
+
+    )
     #created = models.DateTimeField(auto_now_add=True)
     #modified = models.DateTimeField(auto_now=True)
 
