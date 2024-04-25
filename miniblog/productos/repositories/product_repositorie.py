806f980c8b70c0b8ea834e5b42d03f9e883e3bb4
joaquinmartinez2912 <jo.martinez@itecriocuarto.org.ar>
2024-04-25 20:37:42 -0300

    repositories

--- miniblog/productos/repositories/product_repositorie.py
+++ miniblog/productos/repositories/product_repositorie.py
@@ -1,3 +1,4 @@
+#import logging
 from typing import (
     List,
     Optional
@@ -5,6 +6,8 @@ from typing import (
 
 from productos.models import Product, Category
 # Es la carpeta Productos
+
+#logger = logging.getLogger(__name__)
 
 class ProductRepository:
     # Metodo que trae todos los productos
@@ -15,27 +18,49 @@ class ProductRepository:
     def create(
         self,
         nombre: str,
-        descripcion: str,
         precio:float,
-        stocks: int,
+        descripcion: Optional[str] = None,
+        stocks: Optional[int] = None,
         categoria: Optional[Category] = None,
     )-> Product.objects:
         return Product.objects.create(
             name = nombre,
-            description = descripcion,
             price = precio,
-            category = categoria,
+            description = descripcion,
             stock = stocks,
+            category = categoria,
         )
         
         # filtro_por_attr_1 = modelo.objects.filter(attr_1="valor")
-    def filterById(
+    def filter_by_id(
             self,
             atributo:int,
     )   -> Optional[Product]:
         return Product.objects.filter(id=atributo).first()
     
-    def filerByCategoryId(
+    def get_by_id(self, id: int) -> Optional[Product]:
+        try:
+            product = Product.objects.get(id=id)
+        except:
+            product = None
+        return product
+
+    def get_product_on_price_range(
+        self,
+        min_price: float,
+        max_price: float,
+    ) -> List[Product]:
+        #products = Product.objects.filter(
+        #    price__gt=min_price,
+        #    price__lt=max_price,
+        #)
+        products = Product.objects.filter(
+            price__range=(min_price, max_price)
+        )
+
+        return products
+
+    def filer_by_category_id(
             self,
             idCategory = int,
     ) -> Optional[Product]:
@@ -44,9 +69,31 @@ class ProductRepository:
             return Product.objects.filter(category=categoria)
         return []    
     
-    def filerByCategoryId(
+    def filter_by_category_name(
+        self,
+        nombre_categoria: str,
+    ) -> List[Product]:
+        return Product.objects.filter(
+            category__name=nombre_categoria
+        )
+
+    def filer_by_category(
             self,
             category = Category,
     ) -> Optional[Product]:
         return Product.objects.filter(category= category)
+
+    def detele(self, producto: Product):
+        return producto.delete()
+
+    def get_product_gte_stock(
+        self,
+        cantidad_referencia = int,  
+    ) -> Optional[Product]:
+        return Product.objects.filter(stock__gt=cantidad_referencia)
     
+    def get_product_lte_stock(
+        self,
+        cantidad_referencia = int,  
+    ) -> Optional[Product]:
+        return Product.objects.filter(stock__lt=cantidad_referencia) 
